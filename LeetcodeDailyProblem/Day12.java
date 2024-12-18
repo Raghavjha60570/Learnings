8274f30ce6eca3e3e48e83b98a5c5d0667945b6c
Melisandre_64 <Rjha60571@gmail.com>
2024-12-18 20:45:16 +0530

    Update Day12.java
    // printting sequence of fibbonacci number

--- LeetcodeDailyProblem/Day12.java
+++ LeetcodeDailyProblem/Day12.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+ public class fibbonaci{
+        public static void main(String args[]){
 
+                Scanner sc = new Scanner ( System.in);
+                int t = sc.nextInt();
+
+          while ( t-->0){
+
+            int n= sc.nextInt()
+            int prev1=1;
+            int prev2=0;
+
+            for (int i=0 ; i<n ; i++){
+              if(i>2){
+                 int curr= prev1+prev2;
+                 prev2= prev1;
+                 prev1=curr;
+               
+                System.out.println(curr + " ");
+              }else if(n==2){
+                Sytsem.out.println(prev1 + " ");
+              }else {
+                System.out.println(prev2 + " ");
+              }
+            }
+          }
+    }
+ }
+                      
